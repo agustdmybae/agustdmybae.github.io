6925c71a9ad108fd3eb6092b8b47eb8e246d31ec
Chi Chen <stephchen55@gmail.com>
2023-06-11 02:40:23 +0800

    新增 js 檔案

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
-import React from "react"
-import ReactDOM from "react-dom"
+var image = document.getElementById('myImage');
+var modal = document.getElementById('myModal');
+var modalImg = document.querySelector('.modal-content img');
+var closeBtn = document.querySelector('.close');
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-)
+image.addEventListener('click', function() {
+  // 當圖片被點擊時，顯示彈出視窗並設置圖片
+  modal.style.display = 'block';
+  modalImg.src = this.src;
+});
 
-ReactDOM.render(navbar, document.getElementById("root"))
+closeBtn.addEventListener('click', function() {
+  // 當關閉按鈕被點擊時，隱藏彈出視窗
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  // 當點擊視窗外的區域時，隱藏彈出視窗
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
