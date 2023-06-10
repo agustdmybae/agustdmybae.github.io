db641b79e1dcfc038a7a8dce03a18ea65ee69e18
Chi Chen <stephchen55@gmail.com>
2023-06-11 03:52:06 +0800

    update

--- test.js
+++ test.js
@@ -1,22 +1,27 @@
-var image = document.getElementById('myImage');
-var modal = document.getElementById('myModal');
-var modalImg = document.querySelector('.modal-content img');
-var closeBtn = document.querySelector('.close');
+// 取得彈出視窗元素
+var modal = document.getElementById("myModal");
+// 取得關閉按鈕元素
+var closeBtn = modal.querySelector(".close");
 
-image.addEventListener('click', function() {
-  // 當圖片被點擊時，顯示彈出視窗並設置圖片
-  modal.style.display = 'block';
-  modalImg.src = this.src;
+// 取得所有的彈出連結元素
+var popupLinks = document.querySelectorAll(".popup-link");
+
+// 迭代彈出連結元素，為每個元素添加點擊事件處理器
+popupLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        e.preventDefault(); // 防止預設的連結行為
+
+        var imgSrc = link.querySelector("img").src; // 取得點擊連結中的圖片 src 屬性值
+        var modalImage = modal.querySelector("#modalImage"); // 取得彈出視窗中的圖片元素
+
+        modalImage.src = imgSrc; // 將圖片 src 屬性值設置為點擊連結中的圖片 src 屬性值
+        modal.style.display = "block"; // 顯示彈出視窗
+    });
 });
 
-closeBtn.addEventListener('click', function() {
-  // 當關閉按鈕被點擊時，隱藏彈出視窗
-  modal.style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-  // 當點擊視窗外的區域時，隱藏彈出視窗
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
+// 點擊關閉按鈕或彈出視窗以外的區域時隱藏彈出視窗
+window.addEventListener("click", function(event) {
+    if (event.target === modal || event.target === closeBtn) {
+        modal.style.display = "none";
+    }
 });
